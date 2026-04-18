9e86975a66b29114dc0288f84f42b515b6ea15a8
muhammadoksbot-create <muhammad.oks.bot@gmail.com>
2026-04-18 16:22:07 +0400

    Update config_okx.py

--- config_okx.py
+++ config_okx.py
@@ -1,4 +1,4 @@
 # config_okx.py
-API_KEY = "ahIPaPeJWpfTNOSrsp"
-SECRET_KEY = "JdaFq4TQLWLTP2me6yla0Ifu83S5g5mbXJoq"
+API_KEY = "lys4MkZRm0lv0Lmwpp"
+SECRET_KEY = "keWrBKyIhiw4JVbg11J3YkWjz1HAWyu8OG8X"
 PASSPHRASE = "kuch_bhi_rakha_ho_to_ignore"  # Bybit ke liye zaroori nahi
