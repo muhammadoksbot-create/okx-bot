f95a2a07cebe60b846268ff1bbce63199971c709
muhammadoksbot-create <muhammad.oks.bot@gmail.com>
2026-04-18 15:01:50 +0400

    Update config_okx.py

--- config_okx.py
+++ config_okx.py
@@ -1,5 +1,5 @@
-API_KEY = "9b865a0a-c9ee-4ee6-8047-39174c464898"
-SECRET_KEY = "A5B183ED51DFC7A89D572BA13D8A25EE"
-PASSPHRASE = "Password@26"
-
-IS_DEMO = False   # Paper trading ke liye hamesha True
+# ============================================================
+#                    BYBIT API CONFIGURATION
+# ============================================================
+API_KEY    = "ahIPaPeJWpfTNOSrsp"       # <--- API Key yahan paste karo
+SECRET_KEY = "JdaFq4TQLWLTP2me6yla0Ifu83S5g5mbXJoq"    # <--- Secret Key yahan paste karo
