3418245f0191289dcb937f88e3176c3a99487c3f
muhammadoksbot-create <muhammad.oks.bot@gmail.com>
2026-04-18 15:17:00 +0400

    Update config_okx.py

--- config_okx.py
+++ config_okx.py
@@ -1,5 +1,4 @@
-# ============================================================
-#                    BYBIT API CONFIGURATION
-# ============================================================
-API_KEY    = "ahIPaPeJWpfTNOSrsp"       # <--- API Key yahan paste karo
-SECRET_KEY = "JdaFq4TQLWLTP2me6yla0Ifu83S5g5mbXJoq"    # <--- Secret Key yahan paste karo
+# config_okx.py
+API_KEY = "ahIPaPeJWpfTNOSrsp"
+SECRET_KEY = "JdaFq4TQLWLTP2me6yla0Ifu83S5g5mbXJoq"
+PASSPHRASE = "kuch_bhi_rakha_ho_to_ignore"  # Bybit ke liye zaroori nahi
